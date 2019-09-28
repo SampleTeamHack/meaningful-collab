868b34c6434149dbe94c919681c26cc1474825f8
Daniel Vera <daveralo@gmail.com>
2019-09-28 10:46:42 -0500

    Initial constants

---
+++ front/src/constants.js
@@ -0,0 +1,19 @@
+export const category = [
+    { label: "Ambiental", value: "AM" },
+    { label: "Salud", value: "SA" },
+    { label: "Educación", value: "ED" },
+    { label: "Igualdad Social", value: "IS" },
+    { label: "Alimentación", value: "AL" },
+    { label: "Vivienda", value: "VI" }             
+  ];
+
+export const resourceType = [
+    { label: "Voluntarios", value: "VO" },
+    { label: "Recurso económico", value: "RE" }      
+  ];  
+
+export const role = [
+    { label: "Voluntario", value: "VO" },
+    { label: "Líder de proyecto", value: "PM" },    
+    { label: "Sponsor", value: "SP" }      
+  ];  
