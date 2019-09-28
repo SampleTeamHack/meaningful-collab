9115b2e3201201e259369e0c41687b43c59706a9
Nicolas Saavedra <hnsaavedraa@unal.edu.co>
2019-09-28 13:18:30 -0500

    Carousel Finish

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Carousel from './components/Carousel.js'
-import Grid from '@material-ui/core/Grid';
 
 function App() {
   return (
