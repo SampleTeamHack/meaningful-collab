732e399c0441486dde2f4dd566bc217b07143904
Edgar Daniel Mogollon <edmv44@gmail.com>
2019-09-28 13:09:54 -0500

    first commit creation project

--- front/src/index.js
+++ front/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Project from './pages/project/Project';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Project />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
